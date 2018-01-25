c4f503405f45c2cdf93f9891ea2263daba8d3dd2
Icaro Oliveira <icaro@vanics.com>
2018-01-24 16:41:13 -0800

    Create Project skeleton

---
+++ MyGIFs/Supporting Files/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MyGIFs
+//
+//  Created by Ícaro Oliveira on 24/01/18.
+//  Copyright © 2018 vanics. All rights reserved.
+//
+
+import Foundation
+
+enum API {
+    static let GiphyBaseURL = ""
+    // Giphy Key Token
+    static let Token = "X3mqhDeyN6Y5hV2tLToHzsCe5356DNmq"
+}
